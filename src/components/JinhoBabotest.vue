6c1dbbbdbf98706ef6679ef53cdd341b26b27903
Soominyim <soo15soo@kakao.com>
2023-07-19 15:50:24 +0900

    Webhooks test

---
+++ src/components/JinhoBabotest.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div>
+        {{진호바보}}
+        <button @click="진호바보()"></button>
+    </div>
+</template>
+<script>
+export default {
+    name: "진호바보",
+    data() {
+        return {
+            진호바보: "진호바보"
+        }
+    },
+    methods: {
+        진호바보() {
+            alert(this.진호바보)
+        }
+    }
+
+}
+</script>
+<style lang="">
+    
+</style>
