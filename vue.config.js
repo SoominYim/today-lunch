3122da498f25386a4ac8496c7cbb025bb96d2c01
Soominyim <soo15soo@naver.com>
2023-07-19 12:02:12 +0900

    github pages 404 error update

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: "/",
+    publicPath: "/today-lunch/",
     outputDir: "./docs",
+    // resolve: {
+    //     alias: {
+    //         "@": fileURLToPath(new URL("./src", import.meta.url)),
+    //     },
+    // },
+    // base: "/today-lunch/",
     // css: {
     //     loaderOptions: {
     //         sass: {
