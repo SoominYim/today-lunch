8db779f429abdc4b9c9044812833b20e008872fa
SoominYim <soo15soo@kakao.com>
2024-01-16 16:39:16 +0900

    nav list-item route active update

--- src/components/TodayHeader.vue
+++ src/components/TodayHeader.vue
@@ -10,25 +10,47 @@
                 </v-btn>
             </v-toolbar>
         </div>
-        <v-navigation-drawer v-model="drawer">
+        <v-navigation-drawer v-model="drawer" color="#424242">
+            <v-list>
+                <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+                <v-divider></v-divider>
+                <v-list-item v-for="(item, index) in menuItems" :key="index" :prepend-icon="item.icon" :to="item.link"
+                    :title="item.title" @click="navigate(item)"
+                    :class="{ 'v-list-item--active': item.link === $route.path }"></v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </v-app>
 </template>
+
 <script>
 export default {
     data() {
         return {
             drawer: false,
-        }
-    }
-}
+            menuItems: [
+                { icon: 'mdi-food', link: '/today-lunch', title: '뭐 먹 지' },
+                { icon: 'mdi-coffee-outline', link: '/today-lunch/coffee', title: '커 피 사 줘' },
+            ],
+        };
+    },
+    methods: {
+        navigate(item) {
+            this.$router.push(item.link);
+        },
+    },
+};
 </script>
+
 <style lang="scss" scoped>
 .container {
     background-color: none;
     height: 100px;
     width: 100vw;
     position: relative;
-    ;
+}
+
+.v-list-item--active {
+    background-color: rgba(255, 255, 255, 0.1);
+    /* or any other color you prefer */
 }
 </style>
